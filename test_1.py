e67248cf45c02f55603ae9129cb34c88aa3304c4
sarthakbhatt100 <sarthakbhatt100@gmail.com>
2025-03-28 19:52:30 +0530

    commit_2

--- test_1.py
+++ test_1.py
@@ -1,2 +1,20 @@
+ls - list
+
+cat filename - give the content in the terminal itself
+
+mkdir foldername - to create a folder
+
 git init - initialise
+
+git add filename - to add specific files 
+git add . - add all files
+
+
+git log - to get all the commit history
+
+git 
+
+
+
+
 
